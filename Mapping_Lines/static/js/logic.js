ef8c090b011040c880dc357fb0b4b6786174ebe4
sarast.jean <sarast.jean@gmail.com>
2020-08-28 10:52:25 -0500

    update

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -1,52 +1,33 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
 
-// An array containing each city's location, state, and population.
-let cities = [{
-	location: [40.7128, -74.0059],
-	city: "New York City",
-	state: "NY",
-	population: 8398748
-  },
-  {
-	location: [41.8781, -87.6298],
-	city: "Chicago",
-	state: "IL",
-	population: 2705994
-  },
-  {
-	location: [29.7604, -95.3698],
-	city: "Houston",
-	state: "TX",
-	population: 2325502
-  },
-  {
-	location: [28.5383, -81.3792],
-	city: "Orlando",
-	state: "FL",
-	population: 300000
-  },
-  {
-	location: [34.0522, -118.2437],
-	city: "Los Angeles",
-	state: "CA",
-	population: 3990456
-  },
-  {
-	location: [41.2565, -95.9345],
-	city: "Omaha",
-	state: "NE",
-	population: 1660272
-  }
+// Coordinates for each point to be used in the line.
+// Coordinates for each point to be used in the polyline.
+let line = [
+	[33.9416, -118.4085],
+	[37.6213, -122.3790],
+	[40.7899, -111.9791],
+	[47.4502, -122.3088]
   ];
 
+  // Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+	color: "yellow"
+  }).addTo(map);
+
+
+// Get data from cities.js
+let cityData = cities;
+
 // Loop through the cities array and create one marker for each city.
-cities.forEach(function(city) {
+cityData.forEach(function(city) {
 	console.log(city)
-	L.marker(city.location)
+	L.circleMarker(city.location, {
+        radius: city.population/100000
+    })
 	.bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
   .addTo(map);
 });
